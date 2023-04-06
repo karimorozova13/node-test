1f0731272e7d360524139a0ee25d879a199ef286
karimorozova13 <k.morozova@tiomarkets.com>
2023-04-06 17:49:19 +0300

    part of writing tests

---
+++ module-tests/IsLeapYear.test.js
@@ -0,0 +1,8 @@
+const isLeapYear = require("./isLeapYear");
+
+// first argument - name of test, what we are testing
+describe("test of isLeapYear function", () => {
+  test("test if year divides by 4 without rest - true", () => {});
+  test("test if year divides by 100,4 without rest -false", () => {});
+  test("test if year divides by 400,100,4 without rest - true", () => {});
+});
