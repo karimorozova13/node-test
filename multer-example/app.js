70a688afa759705110f525a210604b6235aa8870
karimorozova13 <k.morozova@tiomarkets.com>
2023-04-11 11:26:27 +0300

    save images

--- multer-example/app.js
+++ multer-example/app.js
@@ -3,12 +3,15 @@ const cors = require("cors");
 const multer = require("multer");
 const path = require("path");
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const tempDir = path.join(__dirname, "temp");
+const booksDir = path.join(__dirname, "public", "books");
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
@@ -21,15 +24,36 @@ const upload = multer({
   storage: multerConfig,
 });
 
+const books = [];
+
 // upload.array('cover', 8 (maxCount)) - if we send several files
 // upload.fields([{name:'cover',maxCount:2}, {name:'video',maxCount:3}]) - if I'm waiting files from different fields
 
 app.post("/api/books", upload.single("cover"), async (req, res) => {
-  await fs.rename("./temp/cover.jpg", "./public/books/cover.jpg");
-  console.log(req.body);
-  console.log(req.file);
+  const { name } = req.body;
+  const { path: temporaryPath, originalname } = req.file;
+  try {
+    const uploadPath = path.join(booksDir, originalname);
+    const avatar = path.join("books", originalname);
+    const newBook = {
+      name,
+      id: nanoid(),
+      avatar,
+    };
+    await fs.rename(temporaryPath, uploadPath);
+    books.push(newBook);
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.log(error);
+    await fs.unlink(temporaryPath);
+  }
+});
+app.get("/api/books", async (reg, res) => {
+  try {
+    res.status(200).json(books);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.listen(5001, () => {
-  console.log("Files");
-});
+app.listen(5001);
